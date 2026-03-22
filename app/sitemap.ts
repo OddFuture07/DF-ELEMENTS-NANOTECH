fdf87a23a8789afac671af57bf403744d6f7c4fa
OddFuture07 <cdwjzwl@gmail.com>
2026-03-22 18:31:15 +1100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,10 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.dfelements.com.au',
+      url: "https://www.dfelements.com.au",
       lastModified: new Date(),
-    }
-  ]
+    },
+  ];
 }
